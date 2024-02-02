c22f0090332a9abe89a31d92c28333fc31f38ea1
HanVuHoangLong <longcoutinho@gmail.com>
2024-02-02 17:56:47 +0700

    longhvh fix menu re-render when change url

--- src/pages/item-type/index.tsx
+++ src/pages/item-type/index.tsx
@@ -7,24 +7,22 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "@/constants/FnCommon"
 import React from "react";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function ItemTypeComponent() {
 
     return (
         <Page title={PAGE_TITLE.HOME} menuIndex={1}>
             <Box className="item-type-page-content" sx={{ width: "100vw" }}>
-                <p>Item Type Level 1</p>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={10}
-                    label="Age"
-                >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-                <p>Item Type Level 2</p>
+                <p className="item-type-title">Item Type Level 1</p>
+                <Box className="add-item">
+                    <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                </Box>
+                <p className="item-type-title">Item Type Level 2</p>
+                <Box className="add-item">
+                    <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                </Box>
             </Box>
         </Page>
     );
